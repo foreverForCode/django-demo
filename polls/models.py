87729f6f1d436119c2d1b5d8023e32340c9891f2
jorden <freewm@163.com>
2018-09-02 19:02:49 +0800

    finished first editon

---
+++ polls/models.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import datetime
+
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Question(models.Model):
+
+    question_text = models.CharField(max_length=200)
+
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+
+        return self.question_text
+# 是否已经发布 根据 生成时间和当前时间比较
+    def was_published_recently(self):
+
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+
+    choice_text = models.CharField(max_length=200)
+    
+    def __str__(self):
+
+        return self.choice_text
+    votes = models.IntegerField(default=0)
